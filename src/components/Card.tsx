69655b8e60282c04b2255b00af06e2f068ec8938
JolloDede <jollodede@gmail.com>
2023-02-17 15:35:59 +0100

    fix: key not in props

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,13 +3,12 @@ import { MouseEventHandler } from "react";
 interface Props {
     children?: React.ReactNode;
     classname?: string;
-    key?: number;
     onClick?: MouseEventHandler<HTMLDivElement>;
 }
 
-function Card({ children, classname, key, onClick }: Props) {
+function Card({ children, classname, onClick }: Props) {
     return ( 
-        <div key={key} onClick={onClick} className={"flex border rounded-lg p-4 my-1 "+classname}>
+        <div onClick={onClick} className={"flex border rounded-lg p-4 my-1 "+classname}>
             {children}
         </div>
      );
